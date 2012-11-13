b97884a63593f5a75dbba0aa3f8aff6abe9a6edd
Andrew <andrew.andrew.epifano0@gmail.com>
2012-11-12 20:03:31 -0500

    Commit like a boss :D

--- src/com/CC/Game/Game.java
+++ src/com/CC/Game/Game.java
@@ -1,7 +1,17 @@
 package com.CC.Game;
 
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
+
 public class Game
 {
+	public enum Team {
+		 RED, BLUE;  
+		}
+			//Player        Arena   Team
+	HashMap<Player, HashMap<String, Team>>  gameData =  new HashMap<Player, HashMap<String, Team>>();
+	
     public Game()
     {
     }
