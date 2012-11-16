39fd4bbe4b668cc97f7fae2d02c3e82022cc234f
Mark Hendriks <datfireblast709@gmail.com>
2012-11-17 00:09:35 +0100

    Fix for party commands, and debug
    see above

--- src/com/CC/Commands/PartiesCommands.java
+++ src/com/CC/Commands/PartiesCommands.java
@@ -10,41 +10,64 @@ import com.CC.Parties.*;
 public class PartiesCommands {
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) { 
-		if(sender instanceof Player) {
+		if(sender instanceof Player) 
+        {
 			Player player = (Player)sender;
-			if (cmd.getName().equalsIgnoreCase("party")) {
-				if (args.length == 1){
-				if (args[0].equalsIgnoreCase("help")) {
-					Help.help(player);
-				} else if (args[0].equalsIgnoreCase("create")){
-					Create.create(player);
-				} else if (args[0].equalsIgnoreCase("join")){
-					Join.join(player);
-				} else if (args[0].equalsIgnoreCase("leave")){
-					Leave.leave(player);
-				} else if (args[0].equalsIgnoreCase("stats") || args[0].equalsIgnoreCase("status")) {
-					Status.status(player);
-				} else if (args[0].equalsIgnoreCase("start")){
-					if (args.length == 2 && args[1].equalsIgnoreCase("red")){
-					StartRed.start(player);
-					}else if (args.length == 2 && args[1].equalsIgnoreCase("blue")){
-						StartBlue.start(player);
-						
-					}
-				}
-
-			
+			if (cmd.getName().equalsIgnoreCase("party")) 
+            {
+				if (args.length == 1)
+                {
+                    if (args[0].equalsIgnoreCase("help")) 
+                    {
+                        player.sendMessage("you did /party help");
+                        Help.help(player);
+                    } 
+                    else if (args[0].equalsIgnoreCase("create"))
+                    {
+                        player.sendMessage("you did /party create");
+                        Create.create(player);
+                    } 
+                    else if (args[0].equalsIgnoreCase("join"))
+                    {
+                        player.sendMessage("you did /party join");
+                        Join.join(player);
+                    } 
+                    else if (args[0].equalsIgnoreCase("leave"))
+                    {
+                        player.sendMessage("you did /party leave");
+                        Leave.leave(player);
+                    } 
+                    else if (args[0].equalsIgnoreCase("stats") || args[0].equalsIgnoreCase("status"))
+                    {
+                        player.sendMessage("you did /party stats");
+                        Status.status(player);
+                    } 
+                    else if (args[0].equalsIgnoreCase("start"))
+                    {
+                        player.sendMessage("you did /party start");
+                        if (args.length == 2 && args[1].equalsIgnoreCase("red"))
+                        {
+                            StartRed.start(player);
+                        }
+                        else if (args.length == 2 && args[1].equalsIgnoreCase("blue"))
+                        {
+                            StartBlue.start(player);
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    return true;
+                }
+			}
 			
 			
 			// instance of check because NOT all senders are players. Simply typecasting to player would break on console cmd.
-		}else{
-			
 		}
-		
-		
-		
-
-			}
+        else
+        {
+			// Tho, wtf is happening here? not much ;D
 		}
 		return false;
 	}
