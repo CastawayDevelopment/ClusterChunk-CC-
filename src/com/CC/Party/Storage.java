995bce23952a705f932495883ba6f1c1802881de
Carter Gale <ktar5@live.com>
2012-11-21 10:59:29 -0800

    quick commit

--- src/com/CC/Party/Storage.java
+++ src/com/CC/Party/Storage.java
@@ -1,9 +1,36 @@
 package com.CC.Party;
-
+ 
 import java.util.HashMap;
-
+ 
 public class Storage {
-
-	public HashMap<String, Party> parties = new HashMap<String, Party>();
-	
+ 
+    public HashMap<String, Party> parties = new HashMap<String, Party>();
+    
+    public Party getParty(String name)
+    {
+        // Can return null, be aware
+        return parties.get(name);
+    }
+    
+    public void addParty(String name)
+    {
+        if(!parties.containsKey(name))
+        {
+            parties.put(name, new Party());
+        }
+        else
+        {
+            //player.sendMessage("party already exists");
+        }
+    }
+    
+    public void removeParty(String name)
+    {
+        if(parties.containsKey(name))
+        {
+            parties.remove(name);
+            //player.sendMessage("party removed");
+        }
+    }
+        
 }
