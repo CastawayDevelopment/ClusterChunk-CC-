990051e9b995d9d182c527ea57fd05f9b61b99f3
Ktar5 <ktar5@live.com>
2013-01-18 11:08:30 -0800

    Update src/com/CC/Messages/PlayerMessages.java
    Edited messages, needs reformattinglater.

--- src/com/CC/Messages/PlayerMessages.java
+++ src/com/CC/Messages/PlayerMessages.java
@@ -17,18 +17,6 @@ public class PlayerMessages {
 		return new StringBuilder(GRAY.toString()).append("Sorry ").append(player.getDisplayName()).append(", the world you have entered does not exist.").toString(); 
 	}
 	
-	public String signCategoryDoesNotExist(String string){
-		return new StringBuilder(GRAY.toString()).append("The extension, ").append(string).append("is not valid please use: Join or Status").toString(); 
-	}
-	public String teamColorException(String string){
-		return new StringBuilder(GRAY.toString()).append("Please use either Blue or Red not ").append(string).toString(); 
-	}
-	public String joinSignCreated(String string){
-		return new StringBuilder(GRAY.toString()).append("Congratulations! The join sign for the team ").append(string).append(" has been created").toString(); 
-	}
-	public String statusSignCreated(String string){
-		return new StringBuilder(GRAY.toString()).append("Congratulations! A status sign has been created").toString(); 
-	}
 	public String gameEndedSuccessfully(){
 		return new StringBuilder(GREEN.toString()).append("You have successfully ended the game").toString();
 	}
