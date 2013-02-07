ec607fcfb3806bb73a4eb990f2a4ca999a012b3d
Andrew <andrew.andrew.epifano0@gmail.com>
2013-02-06 20:37:07 -0500

    Commit for Carter

--- src/com/CC/Commands/Party/Versus.java
+++ src/com/CC/Commands/Party/Versus.java
@@ -1,5 +1,13 @@
 package com.CC.Commands.Party;
 
+import com.CC.General.onStartup;
+
 public class Versus {
+	
+	public onStartup plugin;
+	
+	public Versus(onStartup instance){
+		this.plugin = instance;
+	}
 
 }
