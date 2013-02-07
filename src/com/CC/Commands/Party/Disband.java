b52ba2bf618f51b24601a47ee02f5acf5b4f3a9d
Andrew <andrew.andrew.epifano0@gmail.com>
2013-02-06 20:32:01 -0500

    Text changes and Stuff in Party commands

--- src/com/CC/Commands/Party/Disband.java
+++ src/com/CC/Commands/Party/Disband.java
@@ -2,6 +2,8 @@ package com.CC.Commands.Party;
 
 import com.CC.General.onStartup;
 import com.CC.Party.Storage;
+
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class Disband {
@@ -18,6 +20,8 @@ public class Disband {
 	public void disbandParty(Player from){
 		if(partymanager.getParty(from) != null){
 			partymanager.disbandParty(from, partymanager.getParty(from));
+		}else{
+			from.sendMessage(ChatColor.RED + "You are not currently a leader of any parties");
 		}
 		
 		
--- src/com/CC/Commands/Party/Help.java
+++ src/com/CC/Commands/Party/Help.java
@@ -1,33 +1,33 @@
 package com.CC.Commands.Party;
 
 import com.CC.General.onStartup;
-import static org.bukkit.ChatColor.*;
+
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class Help 
 {
-	private onStartup plugin;
+	
     
     public Help(onStartup p)
     {
-        this.plugin = p;
+        
     }
 
 	public void help(Player player){
             
-		player.sendMessage("");
-		player.sendMessage(new StringBuilder(DARK_GRAY.toString()).append("===========").append(DARK_RED).append("[").append(DARK_AQUA).append("Party Commands").append(DARK_RED).append("]").append(DARK_GRAY).append("===========").toString());
-		player.sendMessage("");
-		player.sendMessage(new StringBuilder("/Party Create <party name>").append(" - ").append("Create a party with the specified name").toString());
-		player.sendMessage(new StringBuilder("/Party Help").append(" - ").append("Take a guess <3").toString());
-		player.sendMessage(new StringBuilder("/Party Invite <player name>").append(" - ").append("Invite a player to your closed party").toString());
-		player.sendMessage(new StringBuilder("/Party Join").append(" - ").append("Join a party you were invited to").toString());
-		player.sendMessage(new StringBuilder("/Party Leave").append(" - ").append("Leave the party you are currently in").toString());
-		player.sendMessage(new StringBuilder("/Party Start <red/blue>").append(" - ").append("Start a match with your party on red or blue team").toString());
-		player.sendMessage(new StringBuilder("/Party Status").append(" - ").append("Get the status of your party").toString());
-		player.sendMessage("");
-		player.sendMessage(new StringBuilder(DARK_GRAY.toString()).append("===========").append(DARK_RED).append("[").append(DARK_AQUA).append("Party Commands").append(DARK_RED).append("]").append(DARK_GRAY).append("===========").toString());
-		player.sendMessage("");
+		player.sendMessage(ChatColor.DARK_GRAY + "===========" + ChatColor.DARK_RED + "[" + ChatColor.DARK_AQUA + "Party Commands" + ChatColor.DARK_RED + "]" + ChatColor.DARK_GRAY + "===========");
+		player.sendMessage(ChatColor.DARK_GREEN + "/Party Create <party name>" + ChatColor.GREEN  + " - Create a party with the specified name");
+		player.sendMessage(ChatColor.DARK_GREEN + "/Party Disband " + ChatColor.GREEN  + " - Disband your party");
+		player.sendMessage(ChatColor.DARK_GREEN + "/Party Help " + ChatColor.GREEN  + "- Take a guess <3");
+		player.sendMessage(ChatColor.DARK_GREEN + "/Party Invite <player name> " + ChatColor.GREEN  + "- Invite a player to your closed party");
+		player.sendMessage(ChatColor.DARK_GREEN + "/Party AcceptInvite " + ChatColor.GREEN  + "- Join a party you were invited to");
+		player.sendMessage(ChatColor.DARK_GREEN + "/Party Leave " + ChatColor.GREEN  + "- Leave the party you are currently in");
+		player.sendMessage(ChatColor.DARK_GREEN + "/Party Start <red/blue> " + ChatColor.GREEN  + "- Start a match with your party on red or blue team");
+		player.sendMessage(ChatColor.DARK_GREEN + "/Party Status " + ChatColor.GREEN  + "- Get the status of your party");
+		player.sendMessage(ChatColor.DARK_GREEN + "/Party Versus <PartyName> " + ChatColor.GREEN  + "- Challenge another party");
+		player.sendMessage(ChatColor.DARK_GRAY + "===========" + ChatColor.DARK_RED + "[" + ChatColor.DARK_AQUA + "Party Commands" + ChatColor.DARK_RED + "]" + ChatColor.DARK_GRAY + "===========");
+		
 		
 	}
 }
