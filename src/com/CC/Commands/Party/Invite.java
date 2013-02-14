05a2923afe31a5c67e97ddf769210c1ff2e6f871
Carter Gale <ktar5@live.com>
2013-02-13 19:59:22 -0800

    Line 19. Someone tell me what im doing wrong.

--- src/com/CC/Commands/Party/Invite.java
+++ src/com/CC/Commands/Party/Invite.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import com.CC.General.onStartup;
+import com.CC.Listeners.LobbyListener.*;
 
 public class Invite 
 {
@@ -15,7 +16,7 @@ public class Invite
     }
 
     public void invitePlayer(Player from, Player invited){
-    	if(plugin.getParties().getParty(from) != null){
+    	if(plugin.getParties().getParty(from) != null && inLobby(invited) == false){
     		plugin.getParties().getParty(from).invitePlayer(from, invited);
     			invited.sendMessage(from + " has requested that you join his/her party.");
     			invited.sendMessage("To join " + from + "'s party, type" + ChatColor.RED + " /party accept" + ChatColor.WHITE + " withing the next 20 seconds.");
