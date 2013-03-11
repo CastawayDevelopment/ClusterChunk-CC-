94c879166498113bc6855ebbe0865c8c5697ef83
Mark Hendriks <datfireblast709@gmail.com>
2013-03-11 04:41:28 +0100

    (pt2)

--- src/com/CC/Commands/Party/ChangeLeader.java
+++ src/com/CC/Commands/Party/ChangeLeader.java
@@ -4,24 +4,27 @@ import com.CC.General.onStartup;
 import static org.bukkit.ChatColor.*;
 import org.bukkit.entity.Player;
 
-public class Changeleader {
-	
-	private onStartup plugin;
-	
-	public Changeleader (onStartup instance){
-		plugin = instance;
-		
-	}
-	
-	public boolean ChangePartyLeader(Player sender, Player newOwner){
-		if(plugin.getParties().getParty(sender) == null){
-			sender.sendMessage(new StringBuilder(RED.toString()).append("You are not in a party").toString());
-			return false;
-		}else{
-			return plugin.getParties().getParty(sender).newOwnerShip(sender, newOwner);
-		}
-	}
+public class ChangeLeader
+{
 
-	
+    private onStartup plugin;
 
+    public ChangeLeader(onStartup instance)
+    {
+        plugin = instance;
+
+    }
+
+    public boolean ChangePartyLeader(Player sender, Player newOwner)
+    {
+        if (plugin.getParties().getParty(sender) == null)
+        {
+            sender.sendMessage(new StringBuilder(RED.toString()).append("You are not in a party").toString());
+            return false;
+        }
+        else
+        {
+            return plugin.getParties().getParty(sender).declareLoader(sender, newOwner);
+        }
+    }
 }
