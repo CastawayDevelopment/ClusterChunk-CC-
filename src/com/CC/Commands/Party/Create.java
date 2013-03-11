b8f5bba96f1b9d52a5868eb0f12500a3536082b3
Mark Hendriks <datfireblast709@gmail.com>
2013-03-11 04:41:33 +0100

    pt3

--- src/com/CC/Commands/Party/Create.java
+++ src/com/CC/Commands/Party/Create.java
@@ -4,23 +4,27 @@ import com.CC.General.onStartup;
 import static org.bukkit.ChatColor.*;
 import org.bukkit.entity.Player;
 
-public class Create 
+public class Create
 {
+
     private onStartup plugin;
-    
+
     public Create(onStartup p)
     {
         this.plugin = p;
     }
 
-	public void create(Player player, String partyName) 
+    public void create(Player player, String partyName)
     {
-        plugin.getParties().addParty(partyName, player);
-        if(plugin.getParties().getParty(partyName) != null){
-        	player.sendMessage(new StringBuilder(GREEN.toString()).append("You have succesfully created the party ").append(DARK_GREEN).append(partyName).toString());
-        }else{
-        	player.sendMessage(new StringBuilder(RED.toString()).append("A party with that name already exists. Please choose another.").toString());
+        plugin.getParties().createParty(partyName, player);
+        if (plugin.getParties().getParty(partyName) != null)
+        {
+            player.sendMessage(new StringBuilder(GREEN.toString()).append("You have succesfully created the party ").append(DARK_GREEN).append(partyName).toString());
         }
-        
-	}
+        else
+        {
+            player.sendMessage(new StringBuilder(RED.toString()).append("A party with that name already exists. Please choose another.").toString());
+        }
+
+    }
 }
--- src/com/CC/Commands/Party/Disband.java
+++ src/com/CC/Commands/Party/Disband.java
@@ -6,26 +6,29 @@ import com.CC.Party.Storage;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
-public class Disband {
-	
-	private onStartup plugin;
-	private Storage partymanager;
-	
-	public Disband(onStartup plugin){
-		this.plugin = plugin;
-		partymanager = plugin.getParties();
-		
-	}
-	
-	public void disbandParty(Player from){
-		if(partymanager.getParty(from) != null){
-			partymanager.disbandParty(from, partymanager.getParty(from));
-		}else{
-			from.sendMessage(ChatColor.RED + "You are not currently a leader of any parties");
-		}
-		
-		
-		
-	}
+public class Disband
+{
 
+    private Storage partymanager;
+
+    public Disband(onStartup plugin)
+    {
+        partymanager = plugin.getParties();
+
+    }
+
+    public void disbandParty(Player from)
+    {
+        if (partymanager.getParty(from) != null)
+        {
+            partymanager.disbandParty(from, partymanager.getParty(from));
+        }
+        else
+        {
+            from.sendMessage(ChatColor.RED + "You are not currently a leader of any parties");
+        }
+
+
+
+    }
 }
